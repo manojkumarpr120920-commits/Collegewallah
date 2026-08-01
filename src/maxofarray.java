55938c1edc1f5fd80ba8e2fa7755a4018da8f777
manojkumarpr120920-commits <manojkumarpr120920@gmail.com>
2026-08-01 23:52:49 +0530

    hi

---
+++ src/maxofarray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class maxofarray {
+
+    static int maximum(int arr[]) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Maximum element is: " + maximum(arr));
+
+        sc.close();
+    }
+}
