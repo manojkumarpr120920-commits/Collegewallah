dac29b5f04555a41a7fd04514245aa33c4def6a6
manojkumarpr120920-commits <manojkumarpr120920@gmail.com>
2026-08-06 18:51:04 +0530

    Refactor sorting method to use two pointers

--- src/SortZeroesAndOnesTwoPointers.java
+++ src/SortZeroesAndOnesTwoPointers.java
@@ -1,31 +1,39 @@
 import java.util.Scanner;
 
-public class SortZeroesAndOnes {
+public class SortZeroesAndOnesTwoPointers {
 
-    // Method to sort 0s and 1s
+    // Swap two elements
+    static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Sort using two pointers
     static void sortZeroesAndOnes(int arr[]) {
 
-        int n = arr.length;
-        int zeroes = 0;
+        int left = 0;
+        int right = arr.length - 1;
 
-        // Count the number of zeroes
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                zeroes++;
+        while (left < right) {
+
+            if (arr[left] == 1 && arr[right] == 0) {
+                swap(arr, left, right);
+                left++;
+                right--;
             }
-        }
 
-        // Fill the array
-        for (int i = 0; i < n; i++) {
-            if (i < zeroes) {
-                arr[i] = 0;
-            } else {
-                arr[i] = 1;
+            if (arr[left] == 0) {
+                left++;
+            }
+
+            if (arr[right] == 1) {
+                right--;
             }
         }
     }
 
-    // Method to print the array
+    // Print array
     static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -37,12 +45,13 @@ public class SortZeroesAndOnes {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the size of the array: ");
+        System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
         int arr[] = new int[n];
 
-        System.out.println("Enter the array elements (0s and 1s only):");
+        System.out.println("Enter array elements (0s and 1s only):");
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
