f1237595428ee00c91fa4c34330bed432ad5f807
manojkumarpr120920-commits <manojkumarpr120920@gmail.com>
2026-09-01 15:58:51 +0530

    hi

---
+++ src/FindPairsWithGivenSum.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class FindPairsWithGivenSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the value of x: ");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == x) {
+                    System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
+
